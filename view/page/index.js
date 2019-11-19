590905a84eaa7c24314a490f00d69cff1805f0c9
zss <192626310@qq.com>
2019-11-19 21:45:46 +0800

    add home page name display

---
+++ view/page/index.js
@@ -0,0 +1,22 @@
+layui.use(['form','layer','laydate','table','laytpl'],function(){
+    var form = layui.form,
+        layer = parent.layer === undefined ? layui.layer : top.layer,
+        $ = layui.jquery,
+        laydate = layui.laydate,
+        laytpl = layui.laytpl,
+        table = layui.table;
+
+    $.ajax({
+            url : rootUrl+"/user/userInfo",
+            dataType:"JSON",
+            type : "post",
+            data : {},
+            async : false,
+            success : function(res)
+            {
+                $(".adminName").text(res['data'].name);
+                $(".userName").text(res['data'].name);
+            }
+    });
+
+})
